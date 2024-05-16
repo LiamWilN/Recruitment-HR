a7609fa702f6f7548b44f26924629ab28cc67dd8
William Calda <71976287+LiamWilN@users.noreply.github.com>
2024-05-16 17:56:56 +0800

    Header Changes and Add Contents

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,49 @@
 import React from 'react'
 import { LogoImage } from './Image'
 import LogoURL from '../assets/rmi_logo.png'
-const LogoAlt = "Readyman, Inc."
-import { HeadContainer } from './Container'
+import { X, Menu } from 'lucide-react'
+import DetailContents from '../assets/contents.details.json'
+import { useState } from 'react'
 
 const Header = () => {
 
+    const [currentMenu, menuState] = useState(false)
 
+    const LogoAlt = "Readyman, Inc."
 
   return (
-    <div className='w-screen bg-blue-300 h-24'>
-        <HeadContainer>
-            <div className='flex items-center'>
+    <div className='w-full border-b-2 border-sky-400 h-24'>
+        <div className='container mx-auto h-full flex items-center justify-between'>
+            <div className='flex items-center gap-2 ml-2'>
                 <LogoImage props = {
                     {
                         URL: LogoURL,
                         ALT: LogoAlt
                     }
                 }  />
+
+                <h1 className='font-bold text-xl'>
+                    Readyman, Inc.
+                </h1>
             </div>
-        </HeadContainer>
+            <div className='hidden md:flex items-center content-center'>
+                {
+                    DetailContents.NavigationLinks.map( (i) => {
+                        return (
+                            <button key={i.id} className='w-26 gap-2 mx-4'>{i.title}</button>
+                        )
+                    })
+                }
+            </div>
+
+            <div className='flex md:hidden'>
+                <button onClick={ () => { menuState(!currentMenu) }}>
+                    {
+                        currentMenu ? <X className='mr-2'/> : <Menu className='mr-2' />
+                    }
+                </button>
+            </div>
+        </div>
     </div>
   )
 }
