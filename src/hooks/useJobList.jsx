3a78246f8ef20291114f9d27aef7712584ecb11e
William Calda <71976287+LiamWilN@users.noreply.github.com>
2024-05-31 16:50:58 +0800

    Added Custom Hook

---
+++ src/hooks/useJobList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const useJobList = () => {
+  return <div>useJobList</div>;
+};
+
+export default useJobList;
--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import NotFound from "./pages/NotFound.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <NotFound />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
